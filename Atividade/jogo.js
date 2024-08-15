c0019d86b911ddf96174fd873250df0f1ec0b661
LucasDiSilva <ldias5000@gmail.com>
2024-08-14 21:54:15 -0300

    vai

---
+++ Atividade/jogo.js
@@ -0,0 +1,12 @@
+const Cards = [
+
+"<img alt='javascript' src='/img/1.jpg'>",
+"<img alt='javascript' src='/img/2.png'>",
+"<img alt='javascript' src='/img/3.jpg'>",
+"<img alt='javascript' src='/img/4.jpg'>",
+"<img alt='javascript' src='/img/5.jpg'>",
+"<img alt='javascript' src='/img/6.jpg'>",
+];
+
+let Atual = [];
+
